9dd939ebd5c5663001f14aa82843d6c10acbeb02
Paul Brabban <paul.brabban@gmail.com>
2022-04-03 22:16:15 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ setup_pyenv () {
 	cat << EOF >> ~/.bashrc
  
 #### pyenv support
-export PATH="~/.pyenv/bin:\$PATH"
+export PATH="~/.pyenv/shims:~/.pyenv/bin:\$PATH"
 eval "\$(pyenv init -)"
 eval "\$(pyenv virtualenv-init -)"
 #### done
@@ -32,6 +32,23 @@ install_python () {
 	pyenv rehash
 }
  
+install_vscode () {
+ 
+	if yum list installed code; then 
+		echo "VSCode already installed, skipping..."
+		return 0
+	fi
+ 
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    yum check-update
+	sudo yum install code
+}
+ 
+install_vscode
+ 
 setup_pyenv
 install_python 3.8.13
 python --version
+ 
+ 
