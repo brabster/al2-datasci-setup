3bc79952ac0acd677154c11ddafe68ce65941647
Paul Brabban <paul.brabban@gmail.com>
2022-05-07 07:59:37 +0100

    Init CI pipeline

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ install_vscode () {
 	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     yum check-update
-	sudo yum install code
+	sudo yum install -y code
 }
 
 sudo yum update -y
